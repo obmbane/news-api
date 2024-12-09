d4a05d8342b7bfd603a9c869c94a215a3bab1ad5
obmbane <obmbaneaws@gmail.com>
2024-12-09 20:29:39 +0200

    added URLs, english only articles, added email subject

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests as rq
 import smtplib, ssl, os
 
 api_key = '99c9d45eaef445e08fe288cced78c6b5'
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-11-09&sortBy=publishedAt&apiKey=99c9d45eaef445e08fe288cced78c6b5'
+topic = 'tesla'
+language = 'en'
+url = f'https://newsapi.org/v2/everything?q={topic}&from=2024-11-09&sortBy=publishedAt&apiKey={api_key}&language={language}'
 
 request = rq.get(url)
 content = request.json()
@@ -24,13 +26,17 @@ def send_email(message):
     with smtplib.SMTP_SSL(host,port,context=context) as server:
         server.login(username,password)
         server.sendmail(username,receiver,message)
-message = 'Latest News: '
-for article in content['articles']:
+message = "Subject: Daily News"\
+    + '\n'
+count = 1
+for article in content['articles'][:20]:
 
     if article['title'] is not None:
         title = article['title']
         description = article['description']
-        message += 2*'\n' + title + '\n' + description + '\n'
+        article_url = article['url']
+        message += '\n' + str(count) + ')'+' ' + title + '\n'+ description + '\n' + article_url + 2*'\n'
+        count += 1
 
 message = message.encode('utf-8')
 print(message)
